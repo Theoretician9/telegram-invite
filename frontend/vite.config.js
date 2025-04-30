0bed8b44907c61c285dfc0e2404c932bb45ce29d
Theoretician9 <52958533+Theoretician9@users.noreply.github.com>
2025-04-30 12:52:42 +0500

    Update vite.config.js подключены порты и api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',       // чтобы слушать на всех интерфейсах
+    port: 5173,
+    proxy: {
+      // все запросы /api/* будут проксироваться на бэкенд
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
 })
