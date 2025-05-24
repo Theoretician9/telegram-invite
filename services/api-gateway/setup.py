8c3a986d9b2e536924650e75d95f48d119af7b44
Theoretician9 <nikita.f3d@gmail.com>
2025-05-25 00:49:07 +0500

    Auto-commit every 30s

--- services/api-gateway/setup.py
+++ services/api-gateway/setup.py
@@ -1,1 +1,14 @@
- 
+from setuptools import setup, find_packages
+
+setup(
+    name="api_gateway",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "quart",
+        "celery",
+        "redis",
+        "sqlalchemy",
+        "hypercorn"
+    ],
+) 
