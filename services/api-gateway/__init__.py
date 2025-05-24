506887560468aff76cbbffa13ca45825c98c912e
Theoretician9 <nikita.f3d@gmail.com>
2025-05-25 01:36:21 +0500

    Auto-commit every 30s

--- services/api-gateway/__init__.py
+++ services/api-gateway/__init__.py
@@ -1,5 +1,5 @@
-from .app import app
-from .models import Base, InviteLog
-from .tasks.invite import celery, process_invite
+from app import app
+from models.invite import InviteLog
+from tasks.invite import celery, process_invite
 
-__all__ = ['app', 'Base', 'InviteLog', 'celery', 'process_invite'] 
+__all__ = ['app', 'InviteLog', 'celery', 'process_invite'] 
